591ef619d7bb015a3dd370a6fb9b96d94a5eed0a
BohdanQQ <40754203+BohdanQQ@users.noreply.github.com>
2022-04-24 23:56:24 +0200

    move & refactor table parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 import           Data.List
 import           Data.List.Split
-import           Operations                     ( prettyPrintTable )
+import           Operations                     ( parseCsv
+                                                , prettyPrintTable
+                                                )
 import           System.Environment
 import           System.IO
 import           Types                          ( Cell(..)
@@ -13,52 +15,13 @@ import           Types                          ( Cell(..)
 main :: IO ()
 main = do
     args <- getArgs
-    let argsLen            = length args
-    let (path, schemaDesc) = parseArgs args
-    handle   <- openFile path ReadMode
-    contents <- hGetContents handle
-    let rawRows   = lines contents
-    let splitRows = map (splitOn ",") rawRows
-    let header    = head splitRows
-    let body      = tail splitRows
-    let schema    = createDefualtSchema header schemaDesc
-    let schemaCellOrder = map snd schema
-    let tbody     = map (parseWithSchema schemaCellOrder) body
-    putStrLn (show schema)
-    putStrLn (show tbody)
+    let (path, schemaDesc, parserProperty) = parseArgs args
+    handle <- openFile path ReadMode
+    table  <- parseCsv parserProperty schemaDesc handle
     putStrLn ""
-    prettyPrintTable (schema, [tbody])
+    prettyPrintTable table
     hClose handle
 
-parseArgs args | argCount < 2 = error "Invalid Argument Count"
-               | otherwise    = (head args, args !! 1)
+parseArgs args | argCount < 3 = error "Invalid Argument Count"
+               | otherwise    = (head args, args !! 1, args !! 2)
     where argCount = length args
-
--- parses a row according to a schema (order of Cells in a list)
--- errors out on invalid (unconvertible) values
--- SCHEMA -> STRING VALUES OF A ROW -> PARSED ROW
-parseWithSchema :: [Cell] -> [[Char]] -> [Cell]
-parseWithSchema = zipWith parser
-  where
-    parser cellTemplate cellValue = unwrapOrErr
-        (  "Invalid value "
-        ++ cellValue
-        ++ " for the schema type "
-        ++ typeOfCell cellTemplate
-        )
-        (parseCell cellTemplate cellValue)
-
-createDefualtSchema names repr = if valid
-    then zipWith createSchemaCell names repr
-    else error "Invalid schema specification/column names"
-  where
-    valid =
-        all (\c -> c == 'i' || c == 'd' || c == 'b' || c == 's') repr
-            && nub names == names
-            && length names == length repr
-    createSchemaCell name c = case c of
-        'i' -> (name, CInt 0)
-        's' -> (name, CStr "")
-        'd' -> (name, CDouble 0)
-        'b' -> (name, CBool False)
-        _   -> error "Invalid specification"
--- app/Operations.hs
+++ app/Operations.hs
@@ -1,5 +1,14 @@
-module Operations where
-import           Data.List                      ( transpose )
+module Operations
+    ( prettyPrintTable
+    , parseCsv
+    ) where
+import           Data.List                      ( nub
+                                                , transpose
+                                                )
+import           Data.List.Split                ( splitOn )
+import           System.IO                      ( Handle
+                                                , hGetContents
+                                                )
 import           Types
 
 ------------
@@ -40,3 +49,55 @@ prettyPrintRow padding row = do
         (\(cell, pad) -> putStr (padTo pad ' ' (prettyPrintCell cell) ++ "|"))
         (zip row padding)
     putStrLn ""
+
+-----------------
+---TABLES PARSING
+-----------------
+type Delimiter = String
+type SchemaDescription = String
+parseCsv :: Delimiter -> SchemaDescription -> Handle -> IO Table
+parseCsv delimiter schemaDesc handle = do
+    contents <- hGetContents handle
+    let rawRows   = lines contents
+    let splitRows = map (splitOn delimiter) rawRows
+    let header = if null rawRows
+            then error "No rows, schema could not be loaded!"
+            else head splitRows
+    let body            = tail splitRows
+    let schema          = createDefualtSchema header schemaDesc
+    let schemaCellOrder = map snd schema
+    let tbody           = map (parseWithSchema schemaCellOrder) body
+    return (schema, [tbody])
+
+
+createDefualtSchema :: [String] -> String -> [(String, Cell)]
+createDefualtSchema names repr = if valid
+    then zipWith createSchemaCell names repr
+    else error "Invalid schema specification/column names"
+  where
+    valid =
+        all (\c -> c == 'i' || c == 'd' || c == 'b' || c == 's') repr
+            && nub names
+            == names
+            && length names
+            == length repr
+    createSchemaCell name c = case c of
+        'i' -> (name, CInt 0)
+        's' -> (name, CStr "")
+        'd' -> (name, CDouble 0)
+        'b' -> (name, CBool False)
+        _   -> error "Invalid specification"
+
+-- parses a row according to a schema (order of Cells in a list)
+-- errors out on invalid (unconvertible) values
+-- SCHEMA -> STRING VALUES OF A ROW -> PARSED ROW
+parseWithSchema :: [Cell] -> [[Char]] -> [Cell]
+parseWithSchema = zipWith parser
+  where
+    parser cellTemplate cellValue = unwrapOrErr
+        (  "Invalid value "
+        ++ cellValue
+        ++ " for the schema type "
+        ++ typeOfCell cellTemplate
+        )
+        (parseCell cellTemplate cellValue)
