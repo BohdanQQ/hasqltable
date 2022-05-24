d49f7ff49f51c551500db2b29c804c14245deff7
BohdanQQ <40754203+BohdanQQ@users.noreply.github.com>
2022-05-24 15:00:16 +0200

    refactor, comments Operations

--- app/Operations.hs
+++ app/Operations.hs
@@ -26,31 +26,38 @@ import           Types
 --- PRINTING
 ------------
 
-prettyPrintCell (CInt    i) = show i
-prettyPrintCell (CDouble i) = show i
-prettyPrintCell (CStr    i) = i
-prettyPrintCell (CBool   i) = show i
+-- | returns string representation of a cell's value
+strCellValue (CInt    i) = show i
+strCellValue (CDouble i) = show i
+strCellValue (CStr    i) = i
+strCellValue (CBool   i) = show i
 
+-- | prints the table with paddinng and column / header boundaries
 prettyPrintTable :: Table -> IO ()
 prettyPrintTable (schema, rowgroups) = do
     prettyPrintSchema schema colSizes
-    mapM_ (`prettyPrintSingleGroup` colSizes) rowgroups
+    mapM_ (`prettyPrintRow` colSizes) (concat rowgroups)
   where
     schemaColLengths = map (length . fst) schema
-    maxLenPerCol     = map (maximum . map (length . prettyPrintCell))
+    maxLenPerCol     = map (maximum . map (length . strCellValue))
                            (transpose (concat rowgroups))
     colSizes = zipWith max maxLenPerCol schemaColLengths
-
+-- | applies padding to the end of a string to match certain length
 padTo :: Int -> Char -> String -> String -> String
-padTo padding what string extra =
-    extra ++ string ++ replicate (padding - length string) what ++ extra
+padTo padding padChar string extraStr =
+    extraStr
+        ++ string
+        ++ replicate (padding - length string) padChar
+        ++ extraStr
 
 
+-- | prints the schema row and a separatring line
 prettyPrintSchema :: [(String, Cell)] -> [Int] -> IO ()
 prettyPrintSchema schema padding = do
     putStrLn $ intercalate
         "|"
         (zipWith (\s p -> padTo p ' ' (fst s) " ") schema padding)
+    -- separating line is the same, except instead of column name, the - is repeated (lenght name)-times
     putStrLn $ intercalate
         "+"
         (zipWith (\s p -> padTo p '-' (replicate (length (fst s)) '-') "-")
@@ -58,31 +65,28 @@ prettyPrintSchema schema padding = do
                  padding
         )
 
-
-prettyPrintSingleGroup :: [Row] -> [Int] -> IO ()
-prettyPrintSingleGroup rows padding = mapM_ (prettyPrintRow padding) rows
-
-prettyPrintRow :: [Int] -> Row -> IO ()
-prettyPrintRow padding row = putStrLn $ intercalate
+-- | prints a row with paddinng and column boundaries
+prettyPrintRow :: Row -> [Int] -> IO ()
+prettyPrintRow row padding = putStrLn $ intercalate
     "|"
-    (zipWith (\cell pad -> padTo pad ' ' (prettyPrintCell cell) " ") row padding
-    )
+    (zipWith (\cell pad -> padTo pad ' ' (strCellValue cell) " ") row padding)
 
 type QueryResult = Either Table String
-
 
 ------
 --- MAIN QUERY EXECUTOR
 ------
--- executes a query on a table
+-- | executes a query on a table
 execute
     :: [SubQuery]
     -> Either String ([(String, Cell)], [[[Cell]]])
     -> Either String ([(String, Cell)], [[[Cell]]])
-execute query table = foldl exec table reorderedQuery
+execute query wrappedTable = foldl exec wrappedTable reorderedQuery
   where
+    -- reordering (for execution) is needed, see below
     reorderedQuery = reorderQuery query
-    exec (Right table) qry = executeQuery table qry
+    -- exec - execute subquery or propagate error
+    exec (Right table) qry = executeSubquery table qry
     exec (Left  error) _   = Left error
     reorderQuery q = sortBy (\a b -> compare (idQ a) (idQ b)) q
     idQ (Select  _) = 10 -- select as last
@@ -95,36 +99,40 @@ execute query table = foldl exec table reorderedQuery
 ------
 --- SELECT
 ------
-executeQuery
+-- | executes a single subquery on the table
+executeSubquery
     :: ([(String, Cell)], [[[Cell]]])
     -> SubQuery
     -> Either String ([(String, Cell)], [[[Cell]]])
-executeQuery (schema, rowgroups) (Select cols) = if err0 == ""
+executeSubquery (schema, rowgroups) (Select cols) = if err0 == ""
     then Right (newSchema, extractedRows)
     else Left err0
   where
+    -- the task here is to extract indicies of column names (cols) in the schema
+    -- and filter cells from rows on those positions
     schemaCols       = map fst schema
     mbSchemaIndicies = zip cols (map (`elemIndex` schemaCols) cols)
     invalidCols      = map fst (filter (isNothing . snd) mbSchemaIndicies)
     err0             = if null invalidCols
         then ""
-        else "Invalid columns " ++ intercalate "," invalidCols ++ " requested"
-    schemaIndicies = map (\(_, Just x) -> x) mbSchemaIndicies
-    newSchema      = map (schema !!) schemaIndicies
-    extractedRows  = map rowMapper rowgroups
-    rowMapper rows = map (\x -> map (x !!) schemaIndicies) rows
+        else "Invalid columns (" ++ intercalate "," invalidCols ++ ") requested"
+    schemaIndicies          = map (\(_, Just x) -> x) mbSchemaIndicies
+    extractBySchemaIndicies = \x -> map (x !!) schemaIndicies
+    newSchema               = extractBySchemaIndicies schema
+    extractedRows           = map rowMapper rowgroups
+    rowMapper rows = map extractBySchemaIndicies rows
 
 ------
 --- LIMIT
 ------
-executeQuery (schema, rowgroups) (Limit rowCount) =
+executeSubquery (schema, rowgroups) (Limit rowCount) =
     Right (schema, [take (fromInteger rowCount) (concat rowgroups)])
 
 ------
 --- ORDER BY
 ------
 
-executeQuery (schema, rowgroups) (OrderBy (order, columns)) =
+executeSubquery (schema, rowgroups) (OrderBy (order, columns)) =
     case sortedRows of
         (Right res) -> Right (schema, [res])
         (Left  err) -> Left err
@@ -142,7 +150,7 @@ executeQuery (schema, rowgroups) (OrderBy (order, columns)) =
 ------
 --- WHERE
 ------
-executeQuery (schema, rowgroups) (Where expr) = case result of
+executeSubquery (schema, rowgroups) (Where expr) = case result of
     Left  s            -> Left s
     Right filteredRows -> Right (schema, [filteredRows])
 
@@ -150,15 +158,19 @@ executeQuery (schema, rowgroups) (Where expr) = case result of
     rows       = concat rowgroups
     resultRows = map
         (\r ->
+            -- forcing evaulation with [boolAnd (Const (CBool True))]
+            -- ensures that expr evaluates to a boolean expression
             (evalExpr schema r (Operation expr boolAnd (Const (CBool True))), r)
         )
         rows
+    -- this propagates the first error (Left) into tmp
     tmp = foldl (\acc (e, _) -> acc >> e)
                 (Right (CBool True) :: Either String Cell)
                 resultRows
     result = case tmp of
         Left  err -> Left err
         Right _   -> Right
+        -- finally get the rows (snd) for which the expression is true (okResult . fromRightUnsafe . fst)
             (map snd (filter (okResult . fromRightUnsafe . fst) resultRows))
     fromRightUnsafe x = case x of
         Right e -> e
@@ -171,7 +183,7 @@ executeQuery (schema, rowgroups) (Where expr) = case result of
 --- GROUP_BY
 ------
 
-executeQuery t@(schema, rowgroups) (GroupBy columns) = case groupedRows of
+executeSubquery t@(schema, rowgroups) (GroupBy columns) = case groupedRows of
     Left  rg  -> Right (schema, [map head rg])
     Right err -> Left err
     where groupedRows = createGroupsBy t columns
@@ -182,6 +194,7 @@ createGroupsBy (schema, rowGroups) columns = if null err0
     then Left groupedRows
     else Right err0
   where
+    -- pereparation, error checking
     schemaCols       = map fst schema
     mbSchemaIndicies = zip columns (map (`elemIndex` schemaCols) columns)
     invalidCols      = map fst (filter (isNothing . snd) mbSchemaIndicies)
@@ -190,25 +203,26 @@ createGroupsBy (schema, rowGroups) columns = if null err0
         else "Invalid columns " ++ intercalate "," invalidCols ++ " requested"
     colIdcs     = map (fromJust . snd) mbSchemaIndicies
     rows        = concat rowGroups
+    -- most of the work is done here
     groupedRows = Data.List.groupBy (sameOnIdcs colIdcs) rows
+    -- compares two rows based on column values (idcs)
     sameOnIdcs idcs ra rb = all (\i -> (ra !! i) == (rb !! i)) idcs
 
-------
---- EXECUTOR HELPERS
-------
-
+-- | Orders rows by exactly one column, ordering is stable
 orderByOne
     :: [String]
     -> String
-    -> [[Cell]]
+    -> [Row]
     -> (Cell -> Cell -> Either String Ordering)
-    -> Either String [[Cell]]
+    -> Either String [Row]
 orderByOne schema column rows comparer = if isNothing mbColIdx
     then Left $ "Could not find column " ++ column
     else Right sortedRows
   where
+    -- sortBy is stable
     sortedRows = Data.List.sortBy
         (\r1 r2 -> case comparer (r1 !! colIdx) (r2 !! colIdx) of
+            -- parsing error / table corruption, terminate
             Left s -> error
                 ("Table parsed into invalid format! Attempted to compare incompatible types: "
                 ++ s
